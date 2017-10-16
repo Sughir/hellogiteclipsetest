942a8e70db070814add39ae2be7d7048dfb20d3c
hi <hi@Fata1ityZ68PG3>
2017-10-16 16:57:07 +0900

    test

---
+++ GitTest/main/src/gittest/Test2.java
@@ -0,0 +1,35 @@
+/**
+ * Created Date : 2017/10/16 13:57:07
+ */
+package gittest;
+
+/**
+ * @author hi
+ * @version $Id$
+ *
+ * Created Date : 2017/10/16　13:57:07
+ *
+ */
+public class Test2 {
+  /**
+   * int型の値
+   * @since 2017/10/16　13:58:18
+   */
+  private int test;
+
+  /**
+   * @param test int型の値
+   * @since 2017/10/16　13:58:11
+   */
+  public Test2(final int test) {
+    this.test = test;
+  }
+
+  /**
+   * @return int型の値
+   * @since 2017/10/16　13:58:04
+   */
+  public int getTest() {
+    return this.test;
+  }
+}
